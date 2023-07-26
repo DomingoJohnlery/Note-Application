8b9f799ccec7e4462a10fa4e1ff1ec82268fd30e
DomingoJohnlery <domingojohnled@gmail.com>
2023-07-26 22:58:17 +0800

    Second Commit ActivtyMain & ActivityNote Layouts

--- app/src/main/java/com/mycam/noteapp/MainActivity.kt
+++ app/src/main/java/com/mycam/noteapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mycam.noteapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mycam.noteapp.databinding.ActivityMainBinding
@@ -12,5 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnAddNote.setOnClickListener {
+            startActivity(Intent(this,NoteActivity::class.java))
+        }
     }
 }
